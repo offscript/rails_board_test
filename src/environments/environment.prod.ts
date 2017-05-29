29fb52e16dea3ce4e81d696b80df01ede1baa7c1
Thomas Keating <twkeating1@gmail.com>
2017-05-29 17:50:01 -0400

    minus a /

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   token_auth_config: {
-    apiBase: 'https://salty-tundra-58302.herokuapp.com/'
+    apiBase: 'https://salty-tundra-58302.herokuapp.com'
   }
 };
