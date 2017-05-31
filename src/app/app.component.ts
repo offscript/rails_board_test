4d53abe48e5cf4878f8aa443d749c85b3cb1ca1b
Thomas Keating <twkeating1@gmail.com>
2017-05-31 16:01:00 -0400

    Client-Side Validations, Mobile Layout Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Angular2TokenService } from "angular2-token";
-import { environment } from "../environments/environment.prod";
-
+//import { environment } from "../environments/environment.prod"; //change back to production
+import { environment } from "../environments/environment"; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +11,20 @@ import { environment } from "../environments/environment.prod";
 export class AppComponent {
   title = 'app works!';
  constructor(public authToken: Angular2TokenService){
-    this.authToken.init();
+    this.authToken.init(environment.token_auth_config);
+
+    this.authToken.signIn({email: "user@example.com", password: "monkey67"}).subscribe(
+
+        res => {
+
+          console.log('auth response:', res);
+          console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
+          console.log('auth response body:', res.json()); //log the response body to show the user 
+        },
+
+        err => {
+          console.error('auth error:', err);
+        }
+    )
   }
 }
