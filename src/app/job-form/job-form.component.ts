798c96ec560e168f62c286cdc4f9491bae2f5f2a
Thomas Keating <twkeating1@gmail.com>
2017-06-02 14:56:36 -0400

    Add route to homepage on submit

--- src/app/job-form/job-form.component.ts
+++ src/app/job-form/job-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Router } from "@angular/router";
 
 import { Job } from '../job/job';
 import { JobService } from '../services/job.service';
@@ -12,18 +13,20 @@ import { JobService } from '../services/job.service';
 export class JobFormComponent implements OnInit {
 	job = new Job();
 
-  constructor(private jobService: JobService) { }
+  constructor(private jobService: JobService, private router: Router) { }
 
   //on click return all the values from the form to addHero which will 
   //send json to the service 
   sendJob() {
   	console.log(JSON.stringify(this.job));
-  	this.jobService.addJob(this.job).subscribe(
+  	this.jobService.addJob(this.job).subscribe(() => this.router.navigate(['/']),
   		err => {
             // Log errors if any
             console.log(err);
-            });
+            },
+            );
   }
+
   
   ngOnInit() {}
 
